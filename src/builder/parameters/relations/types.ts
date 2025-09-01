eede7517ab777fda46845e52906bc91172a878bc
Peter <peter.placzek1996@gmail.com>
2025-09-01 16:26:12 +0200

    feat: enhance key types (#627)
    * feat: enhance key types
    
    * feat: enhance type from nested key path
    
    * fix: type from nested key path
    
    * feat: enhance recursion of parameter types

--- src/builder/parameters/relations/types.ts
+++ src/builder/parameters/relations/types.ts
@@ -5,12 +5,22 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import type { ArrayItem, NestedResourceKeys, OnlyObject } from '../../../types';
+import type { NestedResourceKeys, PrevIndex } from '../../../types';
 
-export type RelationsBuildInput<T extends Record<PropertyKey, any>> = {
-    [K in keyof T]?: ArrayItem<T[K]> extends OnlyObject<T[K]> ?
-        RelationsBuildInput<ArrayItem<T[K]>> | boolean :
-        never
-} |
-NestedResourceKeys<T>[] |
-NestedResourceKeys<T>;
+export type RelationsBuildInput<
+    T extends Record<PropertyKey, any>,
+    DEPTH extends number = 5,
+> = [DEPTH] extends [0] ?
+    never : {
+        [K in keyof T & string]?: T[K] extends Array<infer ELEMENT> ?
+            (
+                ELEMENT extends Record<PropertyKey, any> ?
+                    RelationsBuildInput<ELEMENT, PrevIndex[DEPTH]> | boolean :
+                    never
+            ) :
+            T[K] extends Record<PropertyKey, any> ?
+                RelationsBuildInput<T[K], PrevIndex[DEPTH]> | boolean :
+                never
+    } |
+    NestedResourceKeys<T>[] |
+    NestedResourceKeys<T>;
